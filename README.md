7744fee11ca856e7a7e1a5b1bd0f76f3f03cac56
Kei-K23 <keiksl2301@gmail.com>
2024-10-18 23:41:10 +0630

    Add Home page screen

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Sorting Algorithm Visualizer
+# Sorting Algorithm Visualizer 📊
 
-**Sorting algorithm visualizer** is a tool with focusing on visualization for sorting algorithms with highly `customizable`, `minimalist design` and `easy to use features` and build with **React** and **NextJS**.
+**Sorting algorithm visualizer** 📊 is a tool with focusing on visualization for sorting algorithms with highly `customizable`, `minimalist design` and `easy to use features` and build with **React** and **NextJS**.
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { bubbleSort } from "@/sorting-algorithms/bubble-sort";
+import { useEffect, useState } from "react";
+
+const algorithms = [{ name: "Bubble Sort", algorithm: bubbleSort }];
+
+export default function HomePage() {
+  const [arr, setArr] = useState<number[]>([]);
+  const [isSorting, setIsSorting] = useState<boolean>(false);
+  const [algorithm, setAlgorithm] = useState(algorithms[0]);
+  const [duration, setDuration] = useState<number>(50);
+  const [arrSize, setArrSize] = useState<number>(50);
+
+  const resetArr = () => {
+    const newArr = Array.from({ length: arrSize }, () =>
+      Math.floor(Math.random() * 100 + 1)
+    );
+
+    setArr(newArr);
+    setIsSorting(false);
+  };
+
+  useEffect(() => {
+    resetArr();
+  }, [arrSize]);
+
+  const startSorting = async () => {
+    setIsSorting(true);
+    await algorithm.algorithm({ arr, setArr, duration });
+    setIsSorting(false);
+  };
+
+  return (
+    <div className="h-full w-full p-8">
+      <h1 className="text-center mt-10 text-3xl font-bold">
+        Welcome to the Sorting Algorithm Visualizer!
+      </h1>
+    </div>
+  );
+}
